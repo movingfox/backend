b5f45f58104b93376a2aff04363242b4519b91a9
Steam Computing Co., Ltd <code@steamcomputing.com>
2020-06-09 12:15:13 +0800

    Apply devise's generator

--- template.rb
+++ template.rb
@@ -39,7 +39,7 @@ end
 
 inject_into_file 'config/database.yml', after: /_production\n/ do
   <<-RUBY
-  host: postgres
+  host:     <%= ENV['POSTGRES_HOST'] %>
   database: <%= ENV['POSTGRES_DB'] %>
   username: <%= ENV['POSTGRES_USER'] %>
   password: <%= ENV['POSTGRES_PASSWORD'] %>
@@ -52,8 +52,15 @@ after_bundle do
   run 'rails generate friendly_id'
   run 'rails generate trestle:install'
   run 'rails generate trestle:auth:install Administrator'
+  run 'rails generate devise:install'
+  run 'rails generate devise User'
+  inject_into_file 'config/initializers/devise.rb', after: /config\.secret_key.*\n/ do
+    <<-RUBY
+  config.secret_key = Rails.application.credentials.secret_key_base
+    RUBY
+  end
   run 'rails generate komponent:install'
   git :init
-  git add: "."
-  git commit: %Q{ -m 'Initial commit' }
+  git add: '.'
+  git commit: %( -m 'Initial commit' )
 end
